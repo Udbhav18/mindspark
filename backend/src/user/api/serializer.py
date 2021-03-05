3801401e1ed5b15eee34fcda2d04d10af6c33205
Stark <udbhavsomani18@gmail.com>
2021-03-05 20:55:33 +0530

    test

--- backend/src/user/api/serializer.py
+++ backend/src/user/api/serializer.py
@@ -1,5 +1,16 @@
 from rest_framework import serializers
 from user.models import Person
+from rest_framework_simplejwt.tokens import RefreshToken
+
+
+def get_tokens_for_user(user):
+    refresh = RefreshToken.for_user(user)
+
+    return {
+        'refresh': str(refresh),
+        'access': str(refresh.access_token),
+    }
+
 
 class PersonSerializer(serializers.ModelSerializer):
     class Meta:
--- backend/src/user/api/urls.py
+++ backend/src/user/api/urls.py
@@ -1,23 +1,16 @@
 from .views import PersonViewSet
+from django.urls import path
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 router = DefaultRouter()
 router.register('', PersonViewSet, basename='person')
 urlpatterns = router.urls
 
-# from django.urls import path
-# from .views import (
-#     ArticleDetailView, 
-#     ArticleListView, 
-#     ArticleCreateView,
-#     ArticleUpdateView,
-#     ArticleDeleteView
-# )
-
-# urlpatterns = [
-#     path('', ArticleListView.as_view()),
-#     path('create', ArticleCreateView.as_view()),
-#     path('<pk>', ArticleDetailView.as_view()),
-#     path('<pk>/update', ArticleUpdateView.as_view()),
-#     path('<pk>/delete', ArticleDeleteView.as_view())
-# ]
+urlpatterns += [
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
