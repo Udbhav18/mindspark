066a4ac2cee00eaf4cb9087d16230c213bde58c8
Stark <udbhavsomani18@gmail.com>
2021-03-06 16:30:21 +0530

    sign up

--- backend/src/user/urls.py
+++ backend/src/user/urls.py
@@ -1,20 +1,23 @@
 from django.urls import path, include
 from django.views.generic import TemplateView
 from django.contrib.auth.views import LogoutView
-from .views import PersonViewSet
+from .views import PersonDetailViewSet, signupView
 from rest_framework.routers import DefaultRouter
+from django.contrib.auth.views import LogoutView
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
 )
 from . import views
 router = DefaultRouter()
-router.register('', PersonViewSet, basename='person')
+router.register('', PersonDetailViewSet, basename='person')
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name="user/index.html")),
     path('accounts/', include('allauth.urls')),
-    path('logout', LogoutView.as_view()),
+    path('logout/', LogoutView.as_view()),
     path('api/', include(router.urls)),
     path('token/', TokenObtainPairView.as_view(), name='token_refresh'),
-    path('login/', views.loginView, name='login')
+    path('login/', views.loginView, name='login'),
+    path('signup/', signupView, name='signup'),
+    # path('logoutall/', LogoutAllView.as_view(), name='auth_logout_all')
 ]
