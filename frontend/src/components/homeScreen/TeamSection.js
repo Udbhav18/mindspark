4f07aaa0696818293324781bc39360227a846100
Rishabh Sood <sood.rishabh@hotmail.com>
2021-03-06 22:04:02 +0530

    ehh

--- frontend/src/components/homeScreen/TeamSection.js
+++ frontend/src/components/homeScreen/TeamSection.js
@@ -26,7 +26,7 @@ function TeamSection() {
                                 </div>
                             </div>
                             <div className="member-info">
-                                <h4>Walter White</h4>
+                                <h4>Udbhav Somani</h4>
                                 <span>Chief Executive Officer</span>
                                 <p>Velit aut quia fugit et et. Dolorum ea voluptate vel tempore tenetur ipsa quae aut. Ipsum exercitationem iure minima enim corporis et voluptate.</p>
                             </div>
@@ -45,7 +45,7 @@ function TeamSection() {
                                 </div>
                             </div>
                             <div className="member-info">
-                                <h4>Sarah Jhonson</h4>
+                                <h4>Neelaksh Tayal</h4>
                                 <span>Product Manager</span>
                                 <p>Quo esse repellendus quia id. Est eum et accusantium pariatur fugit nihil minima suscipit corporis. Voluptate sed quas reiciendis animi neque sapiente.</p>
                             </div>
@@ -64,7 +64,7 @@ function TeamSection() {
                                 </div>
                             </div>
                             <div className="member-info">
-                                <h4>William Anderson</h4>
+                                <h4>Mihir Kumar Roy</h4>
                                 <span>CTO</span>
                                 <p>Vero omnis enim consequatur. Voluptas consectetur unde qui molestiae deserunt. Voluptates enim aut architecto porro aspernatur molestiae modi.</p>
                             </div>
@@ -83,7 +83,7 @@ function TeamSection() {
                                 </div>
                             </div>
                             <div className="member-info">
-                                <h4>Amanda Jepson</h4>
+                                <h4>Aarya Pathania</h4>
                                 <span>Accountant</span>
                                 <p>Rerum voluptate non adipisci animi distinctio et deserunt amet voluptas. Quia aut aliquid doloremque ut possimus ipsum officia.</p>
                             </div>
@@ -93,7 +93,7 @@ function TeamSection() {
                     <div className="col-lg-3 col-md-6 d-flex align-items-stretch" style={{ marginLeft: '50%', transform: 'translateX(-50%)' }} data-aos="fade-up" data-aos-delay="400">
                         <div className="member">
                             <div className="member-img">
-                                <img src="assets/img/team/team-4.jpg" className="img-fluid" alt=""></img>
+                                <img src="assets/img/team/team-5.jpg" className="img-fluid" alt=""></img>
                                 <div className="social">
                                     <Link to=""><i className="bi bi-twitter"></i></Link>
                                     <Link to=""><i className="bi bi-facebook"></i></Link>
@@ -102,7 +102,7 @@ function TeamSection() {
                                 </div>
                             </div>
                             <div className="member-info">
-                                <h4>Amanda Jepson</h4>
+                                <h4>Rishabh Sood</h4>
                                 <span>Accountant</span>
                                 <p>Rerum voluptate non adipisci animi distinctio et deserunt amet voluptas. Quia aut aliquid doloremque ut possimus ipsum officia.</p>
                             </div>
